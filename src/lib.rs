610a188cd262639d66cd2e6da27d8b5a4943584c
A9-dev <henrypearson422@gmail.com>
2025-01-28 10:02:04 +0000

    feat: :sparkles: finish structs and add basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,91 +7,132 @@ use serde_with::serde_as;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde_as]
-pub struct VerifiableCredential {
+#[serde(deny_unknown_fields)]
+struct VerifiableCredential {
     #[serde(rename = "@context")]
-    pub context: Vec<String>,
-    pub id: Option<String>,
+    context: Vec<String>,
+    id: Option<String>,
     #[serde(rename = "type")]
     #[serde_as(as = "OneOrMany<_, PreferOne>")]
-    pub credential_type: Vec<String>,
-    pub name: Option<LanguageValue>,
-    pub description: Option<LanguageValue>,
-    pub issuer: Issuer,
-    pub credential_subject: Value,
-    #[serde(rename = "validFrom")]
-    pub valid_from: Option<DateTime<Utc>>,
-    #[serde(rename = "validUntil")]
-    pub valid_until: Option<DateTime<Utc>>,
-    pub status: Option<Status>,
-    pub credential_schema: Option<CredentialSchema>,
-    pub refresh_service: Option<RefreshService>,
-    pub terms_of_use: Option<Vec<TermsOfUse>>,
-    pub evidence: Option<Vec<Evidence>>,
+    credential_type: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<LanguageValue>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<LanguageValue>,
+    issuer: Issuer,
+    #[serde(rename = "credentialSubject")]
+    credential_subject: Value,
+    #[serde(rename = "validFrom", skip_serializing_if = "Option::is_none")]
+    valid_from: Option<DateTime<Utc>>,
+    #[serde(rename = "validUntil", skip_serializing_if = "Option::is_none")]
+    valid_until: Option<DateTime<Utc>>,
+    #[serde(rename = "credentialStatus", skip_serializing_if = "Option::is_none")]
+    credential_status: Option<Status>,
+    #[serde_as(as = "OneOrMany<_, PreferOne>")]
+    #[serde(rename = "credentialSchema", skip_serializing_if = "Option::is_none")]
+    credential_schema: Option<CredentialSchema>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    proof: Option<Proof>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Issuer {
+#[serde(untagged)]
+enum Issuer {
     String(String),
     Object(IssuerObject),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct IssuerObject {
-    pub id: String,
+struct IssuerObject {
+    id: String,
     #[serde(flatten)]
-    pub additional_properties: Option<HashMap<String, Value>>,
+    additional_properties: Option<HashMap<String, Value>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
-pub enum LanguageValue {
+enum LanguageValue {
     PlainString(String),
     LanguageObject(LanguageObject),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct LanguageObject {
+struct LanguageObject {
     #[serde(rename = "@value")]
-    pub value: String,
-    #[serde(rename = "@language")]
-    pub language: Option<String>,
-    #[serde(rename = "@direction")]
-    pub direction: Option<String>,
+    value: String,
+    #[serde(rename = "@language", skip_serializing_if = "Option::is_none")]
+    language: Option<String>,
+    #[serde(rename = "@direction", skip_serializing_if = "Option::is_none")]
+    direction: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Status {
-    pub id: Option<String>,
+#[serde_as]
+struct Status {
+    #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
+    id: Option<String>,
     #[serde(rename = "type")]
-    pub status_type: String,
+    #[serde_as(as = "OneOrMany<_, PreferOne>")]
+    status_type: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct CredentialSchema {
-    pub id: String,
+#[serde_as]
+struct CredentialSchema {
+    id: String,
     #[serde(rename = "type")]
-    pub schema_type: String,
+    schema_type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct RefreshService {
-    pub id: String,
+#[serde_as]
+struct Proof {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    id: Option<String>,
     #[serde(rename = "type")]
-    pub service_type: String,
+    proof_type: String,
+    #[serde(rename = "proofPurpose")]
+    proof_purpose: String,
+    #[serde(rename = "verificationMethod")]
+    verification_method: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cryptosuite: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    created: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    expires: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde_as(as = "OneOrMany<_, PreferOne>")]
+    domain: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    challenge: Option<String>,
+    #[serde(rename = "proofValue")]
+    proof_value: String,
+    #[serde(rename = "previousProof", skip_serializing_if = "Option::is_none")]
+    previous_proof: Option<String>,
+    #[serde_as(as = "OneOrMany<_, PreferOne>")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    nonce: Option<Vec<String>>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct TermsOfUse {
-    pub id: String,
-    #[serde(rename = "type")]
-    pub terms_type: String,
-    // Additional properties can be added here
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Evidence {
-    pub id: String,
-    #[serde(rename = "type")]
-    pub evidence_type: String,
-    // Additional properties can be added here
+    #[test]
+    fn valid_vc_deserializes() {
+        let vc: VerifiableCredential =
+            serde_json::from_str(include_str!("../test_data/henryTrustPhone.json"))
+                .expect("Failed to deserialize JSON");
+
+        assert!(serde_json::to_string(&vc).is_ok());
+    }
+
+    #[test]
+    fn invalid_vc_fails_to_deserialize() {
+        let vc: Result<VerifiableCredential, _> =
+            serde_json::from_str(include_str!("../test_data/henryTrustPhoneInvalid.json"));
+
+        assert!(vc.is_err());
+    }
 }
